41fa2feaddab23cb14c50379e237b8ff7da39f06
Dominique <info@mchobby.be>
2017-12-29 13:55:25 +0100

    update topics

--- esp8266/cabane/main.py
+++ esp8266/cabane/main.py
@@ -77,19 +77,19 @@ while runapp.value()==1:
     led.value( 1 ) # eteindre pendant envoi
     # tsl2561 - senseur lux
     lux = "{0:.2f}lx".format( tsl.read() )
-    q.publish( "maison/exterieur/cabane/lux", lux )
+    q.publish( "maison/exterieur/cabane/lux", lux ) 
     # bmp280 - senseur pression/température
     (t,p,h) = bmp.values # capturer les valeurs sous format texte
-    q.publish( "maison/exterieur/cabane/pression", p )
-    q.publish( "maison/exterieur/cabane/temperature", t )
+    q.publish( "maison/exterieur/cabane/pathm", p )
+    q.publish( "maison/exterieur/cabane/temp", t )
     # am2315 - humidité/temperature
     am.measure() # reactive le senseur
     sleep( 1 )
     am.measure()
     t = "{0:.2f}C".format( am.temperature() )
     h = "{0:.2f}%Rh".format( am.humidity() )
-    q.publish( "maison/exterieur/jardin/temperature", t )
-    q.publish( "maison/exterieur/jardin/humidite", h )
+    q.publish( "maison/exterieur/jardin/temp", t )
+    q.publish( "maison/exterieur/jardin/hrel", h )
     led.value( 0 ) # allumer
 
     # pause de x sec
