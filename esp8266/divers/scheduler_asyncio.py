d33bffb1a4a7d901deb76fd712789277356dc3fb
mchobby <info@mchobby.be>
2017-12-30 14:23:52 +0000

    task scheduler with asyncio

---
+++ esp8266/divers/scheduler_asyncio.py
@@ -0,0 +1,26 @@
+# coding: utf8
+""" Utilisation de asyncio pour planifier des taches.
+
+  Voir également asyncio et exemples sur
+  https://github.com/peterhinch/micropython-async """
+
+import uasyncio as asyncio
+
+async def print_this( s, time_ms ):
+    while True:
+        await asyncio.sleep_ms( time_ms )
+        print( s )
+
+loop = asyncio.get_event_loop()
+loop.create_task( print_this( "every sec", 1000 ) )
+loop.create_task( print_this( "every 1.5sec", 1200 ) )
+
+loop.run_forever()
+
+#async def killer( sec ):
+#    await asyncio.sleep( sec )
+#
+#loop.run_until_complete( killer( sec=25 ) )
+#print( "Execution terminee")
+
+loop.close()
