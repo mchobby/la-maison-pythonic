1ec2dca32311e7f6c93aea80886bea9667090c99
mchobby <info@mchobby.be>
2018-01-21 11:39:54 +0000

    publication mqtt sous MicroPython

---
+++ esp8266/divers/mqtt_pub.py
@@ -0,0 +1,37 @@
+""" La Maison Pythonic - publication sur broker MQTT depuis MicroPython """ 
+from network import WLAN
+
+import time
+from network import WLAN
+from umqtt.simple import MQTTClient
+from ubinascii import hexlify
+import sys
+
+CLIENT_ID = "demo-pub"
+
+MQTT_SERVER = "192.168.1.210"
+
+# Mettre a None si pas utile
+MQTT_USER = 'pusr103'
+MQTT_PSWD = '21052017'
+
+print( "Creation MQTTClient")
+q = MQTTClient( client_id = CLIENT_ID, server = MQTT_SERVER, user = MQTT_USER, password = MQTT_PSWD )
+
+if q.connect() != 0:
+	print( "erreur connexion" )
+	sys.exit()
+print( "Connecté" )
+
+# annonce connexion objet
+sMac = hexlify( WLAN().config( 'mac' ) ).decode()
+q.publish( "connect/%s" % CLIENT_ID , sMac )
+
+# publication d'un compteur
+for i in range( 10 ):
+	print( "pub %s" % i )
+	q.publish( "demo/compteur", str(i) )
+	time.sleep( 1 ) 
+
+q.disconnect()
+print( "Fin de traitement" )
