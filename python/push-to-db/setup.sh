2f21db13c2fd4aaddf2ee580b5f058277c5e133a
Dominique <info@mchobby.be>
2018-12-15 08:09:04 +0100

    fix push-to-db  setup.sh

--- python/push-to-db/setup.sh
+++ python/push-to-db/setup.sh
@@ -28,23 +28,38 @@ sudo mkdir /var/log/pythonic
 sudo chgrp staff /var/log/pythonic
 sudo chmod g+rw /var/log/pythonic
 
-# reload user configuration (and groups)
-exec su -l $USER
+# Needs to reload user configuration (and groups)
+# so user "pi" can act with "staff" group access 
+# without logout/login again.
+# Executing command with 'sudo su ...' will done it 
+# for the current user "pi" but having "staff" group
+# attached immediately to it 
+# sudo su -c 'command' $USER
+echo "Check user groups, staff must now be attached"
+sudo su -c 'groups' $USER
 
 # owner and group  would be the current user (pi)
-touch /var/log/pythonic/push-to-db.log
+sudo su -c 'touch /var/log/pythonic/push-to-db.log' $USER
 # Set permission to -rw-rw-r--
-sudo chmod +664 /var/log/pythonic/push-to-db.log
+sudo su -c 'sudo chmod +664 /var/log/pythonic/push-to-db.log' $USER
 # change owner to root (keep pi as the group)
-sudo chown root /var/log/pythonic/push-to-db.log
+sudo su -c 'sudo chown root /var/log/pythonic/push-to-db.log' $USER
 
 echo "Creating pythonic.db in var/local/sqlite/"
 # Initialize the pythonic databases to run the push-to-db.py python script. 
 # push-to-db.py push MQTT data to SQLite3 database
-cat createdb.sql | sqlite3 /var/local/sqlite/pythonic.db
+sudo su -c 'cat createdb.sql | sqlite3 /var/local/sqlite/pythonic.db' $USER
 
 echo "Installing python libraries"
 sudo pip install paho-mqtt
 
 echo "Done!"
 
+if $(groups | fgrep -wq -e staff); then
+   echo "Great staff is already in user groups :)"
+else
+   echo "-------------------------------------"
+   echo "You must logout/login to update your"
+   echo "current session user groups !"
+   echo "------------------------------------"
+fi
